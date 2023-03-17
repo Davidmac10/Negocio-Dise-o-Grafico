bb1859c2bde25c6da384ad033d6f4ae81bffe2fa
Davidmac10 <david09192001@gmail.com>
2023-03-17 12:27:03 -0500

    Tercera version se elimina un comentario y se personaliza el mensaje

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ cursor = conexion.cursor()
 
 prefijo = "+57"
     
-codigo_tel = ['300','301','302','303','304','305','306','307','308','309','310','311','312','313','314','315','316','317','318','319','320','321','322','323','324','325','326','327','328','329']
+codigo_tel = ['300','301','302','303','304','305','306','307','308','309','310','311','312','313','314','315','316','317','318','319','320','321','322','323', '350', '351', '352', '353', '354', '355', '356', '357', '358', '359']
 
 complemento = random.randint(1000000, 9999999)
 
@@ -24,7 +24,7 @@ if telefono_search is None:
         
     cursor.execute("INSERT INTO telefono (telefono, estado) VALUES (?, ?)", (numero_celular, estado))
 
-    mensaje = "Hola, mi nombre es David Stiven Mendieta Barajas, soy diseñador grafico, te cuento mi emprendimiento, me dedico a hacer logos para cualquier tipo de negocio, fotomontajes, ilustraciones computarizadas, edicion de video profesional y animaciones 2D, si te interesa adquirir cualquiera de mis servicios, contactame o escribeme al +57 3232012435 para mas informacion" #input("Ingresa tu mensaje a enviar: ")
+    mensaje = input("Ingresa tu mensaje a enviar: ")
     hora = int(input("Ingresa la hora: "))
     minuto = int(input("Ingresa el minuto: "))
         
